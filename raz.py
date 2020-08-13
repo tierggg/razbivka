ec23f4eaf1d3796f87e5ebb7a331ee14acab7a45
ROOT <tierggg11@gmail.com>
2020-08-13 10:44:56 +0300

    1

--- raz.py
+++ raz.py
@@ -3,18 +3,25 @@
 import xlrd
 import xlwt
 from tkinter.filedialog import askopenfilename
-import subprocess
-import os
+from tkinter.filedialog import askdirectory
+from os import startfile
+from os.path import realpath
 
 filename = askopenfilename()
 rb = xlrd.open_workbook(filename)
 sheetR = rb.sheet_by_index(0)
 
-font = xlwt.Font()               # font
-font.name = 'Times New Roman'
-font.height = 11 * 20            # Нужный размер шрифта нужно умножить на 20
+path = askdirectory()
+path = realpath(path)
 
-borders = xlwt.Borders()         # borders
+font = xlwt.Font()                      # font
+font.name = 'Times New Roman'
+font.height = 11 * 20                   # Нужный размер шрифта нужно умножить на 20
+minifont = xlwt.Font()                  # font
+minifont.name = 'Times New Roman'
+minifont.height = 8 * 20                # Нужный размер шрифта нужно умножить на 20
+
+borders = xlwt.Borders()                # borders
 borders.left = 1
 borders.right = 1
 borders.top = 1
@@ -27,34 +34,42 @@ date_style.font = font
 style = xlwt.XFStyle()
 style.borders = borders
 style.font = font
+ministyle = xlwt.XFStyle()
+ministyle.borders = borders
+ministyle.font = minifont
 
 regNumber = set()
 for row in range(3, sheetR.nrows):
     if sheetR.cell_type(row, 9) not in (xlrd.XL_CELL_EMPTY, xlrd.XL_CELL_BLANK):  # Проверка пуста ли ячейка
         regNumber.add(sheetR.cell_value(row, 9))
 
-colsWidths = [3, 16, 26, 27, 24, 12, 4, 85, 7, 22, 13]  # ширина столбцов от 0 до последнего
+# Ширина столбцов от первого до последнего, если ширина 0, столбец будет скрыт
+colsWidths = [3, 16, 26, 27, 24, 12, 4, 85, 7, 22, 13]
 colDict = {}
-
 for k in range(len(colsWidths)):
     colDict[k] = colsWidths[k]
 
 cont = 1
-
 for val in regNumber:
-    print(cont, 'из', len(regNumber))
-    cnt = 2
+    cnt = 3                                             # Номер первой строки после шапки (счёт строк от 0)
     outBook = xlwt.Workbook()
     outSheet = outBook.add_sheet(str(int(val)))
 
     for i in colDict:
-        outSheet.col(i).width = colDict[i] * 256    # Нужную ширину стобца надо умножить на 256
+        if colDict[i] == 0:
+            outSheet.col(i).hidden = 1                  # Скрытие столбца
+        else:
+            outSheet.col(i).width = colDict[i] * 256    # Нужную ширину стобца надо умножить на 256
 
-    for row in range(1, 2):
-        for col in range(sheetR.ncols):
-            outSheet.write(row, col, sheetR.cell_value(row, col), style=style)
+    for row in range(1, cnt):                            # Заполнение шапки
+        if row == 2:
+            for col in range(sheetR.ncols):
+                outSheet.write(row, col, sheetR.cell_value(row, col), style=ministyle)
+        else:
+            for col in range(sheetR.ncols):
+                outSheet.write(row, col, sheetR.cell_value(row, col), style=style)
 
-    for row in range(3, sheetR.nrows):
+    for row in range(cnt, sheetR.nrows):
         if val == sheetR.cell_value(row, 9):
             for col in range(sheetR.ncols):
                 if col == 5:
@@ -62,9 +77,10 @@ for val in regNumber:
                 else:
                     outSheet.write(cnt, col, sheetR.cell_value(row, col), style=style)
             cnt += 1
-    outBook.save('D:\\1\\out\\' + str(int(val)) + '.xls')
+
+    #outBook.save('D:\\1\\out\\' + str(int(val)) + '.xls')
+    outBook.save(path + str(int(val)) + '.xls')
+    print(cont, 'из', len(regNumber))
     cont += 1
 
-path = 'D:\\1\\out\\'
-path=os.path.realpath(path)
-os.startfile(path)
+startfile(path)
